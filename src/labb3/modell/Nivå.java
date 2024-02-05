9d85ed785e83e2159f20f88a7504b6af7ca9b3b8
LukasErikss0n <Lukas.emil.eriksson@outlook.com>
2024-02-05 14:35:34 +0100

    fixade skelet

--- src/labb3/modell/Nivå.java
+++ src/labb3/modell/Nivå.java
@@ -1,16 +1,24 @@
 ﻿package labb3.modell;
 
 import java.util.ArrayList;
+import java.util.Observable;
 
-// TODO: Gör så att klassen Nivå ärver Observable i paketet java.util. 
-public class Nivå {
+// TODO: Gör så att klassen Nivå ärver Observable i paketet java.util.
+public class Nivå extends Observable {
 
 	// TODO: Lägg till tillståndsvariabler för att hålla reda på nivåns rum och
 	// i vilket rum som användaren "är".
+	private Rum aktivt;
+	private ArrayList<Rum> allaRum;
 
 	public Nivå(Rum startrum, ArrayList<Rum> rum) {
 		// TODO: Kopiera in startrum och rum in i tillståndsvariablerna.
+		aktivt = startrum;
+		allaRum = rum;
 
+		for(Rum rum: allaRum){
+			if( rum )
+		}
 		// TODO: Kontrollera att startrum finns med i rum. Om inte, kasta
 		// undantag med lämpligt felmeddelande.
 
